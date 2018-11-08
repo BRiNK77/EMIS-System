8b850b038bef632a8eb96a8b6dbdf4423f5874d1
Triston Scallan <triston.scaln@icloud.com>
2018-11-08 16:01:59 -0600

    Initial commit
    Made commit for the initial file structure. Import as a maven project.

---
+++ emis/src/main/java/com/emissystem/www/home/EmisMain.java
@@ -0,0 +1,18 @@
+package com.emissystem.www.home;
+
+
+
+import org.eclipse.jetty.util.resource.Resource;
+
+
+import static spark.Spark.*;
+
+public class EmisMain {
+    public static void main(String[] args) {
+        //TODO: cannot mix static files and resources from a jar with spark.
+        staticFileLocation("/");
+        Resource resource = Resource.newClassPathResource("/META-INF/resources");
+        get("/list", (req, res) -> resource.getListHTML("/", true));
+        //get("/", (req, res) -> );
+    }
+}
