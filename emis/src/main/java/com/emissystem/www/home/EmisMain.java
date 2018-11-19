19284ab07b549d623b18563cdc4652c3ba8d1532
Gabriel Sotelo-Rosas <m.gabriel.rosas@gmail.com>
2018-11-19 16:51:40 -0600

    Login WIP
    Changed file location in main
    Added comments to explain how to use the server
    
    Added login.html
    
    TODO: Vertical Centering of login container

--- emis/src/main/java/com/emissystem/www/home/EmisMain.java
+++ emis/src/main/java/com/emissystem/www/home/EmisMain.java
@@ -10,7 +10,10 @@ import static spark.Spark.*;
 public class EmisMain {
     public static void main(String[] args) {
         //TODO: cannot mix static files and resources from a jar with spark.
-        staticFileLocation("/");
+        staticFileLocation("/public");
+        // All static files (images, html files) should be placed in "/emis/src/main/resources/public"
+        // When the app/server is running, access it with "localhost:4567" in a browser
+        // To display an html page you made and put in /public, access it with "localhost:4567/FileName.html"
         Resource resource = Resource.newClassPathResource("/META-INF/resources");
         get("/list", (req, res) -> resource.getListHTML("/", true));
         //get("/", (req, res) -> );
