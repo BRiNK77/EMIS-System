73035ce1e7034423f01ce1e3f3344669e5725be7
Triston Scallan <triston.scaln@icloud.com>
2018-12-03 05:21:21 -0600

    styled pages

--- emis/src/main/java/com/emissystem/www/home/EmisMain.java
+++ emis/src/main/java/com/emissystem/www/home/EmisMain.java
@@ -6,13 +6,25 @@ public class EmisMain {
     public static void main(String[] args) {
         //TODO: cannot mix static files and resources from a jar with spark.
         staticFileLocation("/public");
-
+        static int counter = 0;
         // All static files (images, html files) should be placed in "/emis/src/main/resources/public"
         // When the app/server is running, access it with "localhost:4567" in a browser
         // To display an html page you made and put in /public, access it with "localhost:4567/FileName.html"
 
         get("/", (request, response) -> {
             response.redirect("login.html");
+            return null;
+        });
+        get("/main", (request, response) -> {
+            if(counter == 0) {
+              response.redirect("mainDoctor.html");
+              counter++;
+            } else if (counter == 1) {
+              response.redirect("mainReceptionist.html");
+              counter++
+            } else {
+              response.redirect("mainPatient.html");
+            }
             return null;
         });
         //get("/", (req, res) -> );
